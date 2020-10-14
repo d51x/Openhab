e11a8df04802c334af7656583f32327bb154f257
d51x <dimonich110@gmail.com>
2020-10-14 23:11:46 +0300

    14.10.2020

--- scripts/free_hdd.sh
+++ scripts/free_hdd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-FREE=`df -k --output=avail "/home/media" | tail -n1`
+FREE=`df -h --output=avail "/home/media" | tail -n1`
 echo $FREE
---
+++ transform/tempset_from.js
@@ -0,0 +1,4 @@
+(function(i) {
+    var val = parseInt(i) / 10;
+    return parseFloat(val).toFixed(1);
+})(input)
